3eff3a68db6c4887d4c02ae6f3a68b766b15aa6b
chen <m15903462872@163.com>
2019-07-26 10:54:12 +0800

    update code

--- Queue/src/Main.java
+++ Queue/src/Main.java
@@ -1,9 +1,40 @@
+import java.util.Random;
+
 /**
  * @author Calvin
  * @Description:
  */
 public class Main {
+    /**
+     * 测试使用q运行opCount个enqueue和dequeue操作需要的时间，单位：S
+     *
+     * @param q
+     * @param opCount
+     * @return
+     */
+    private static double testQueue(Queue<Integer> q, int opCount) {
+        long startTime = System.nanoTime();
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            q.enqueue(random.nextInt(Integer.MAX_VALUE));
+        }
+        for (int i = 0; i < opCount; i++) {
+            q.dequeue();
+        }
+
+        long endTime = System.nanoTime();
+        return (endTime - startTime) / 1000000000.0;
+    }
+
     public static void main(String[] args) {
+        int opCount = 100000;
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+        double time1 = testQueue(arrayQueue, opCount);
+        System.out.println("ArrayQueue,time:" + time1 + "s");
+
+        LoopQueue<Integer> loopQueue = new LoopQueue<>();
+        double time2 = testQueue(loopQueue, opCount);
+        System.out.println("LoopQueue,time:" + time2 + "s");
        /* ArrayQueue<Integer> queue = new ArrayQueue<>();
         for (int i = 0; i < 10; i++) {
             queue.enqueue(i);
@@ -14,8 +45,9 @@ public class Main {
                 System.out.println(queue);
             }
         }*/
-        LoopQueue<Integer> queue = new LoopQueue<>();
-        for (int i = 0; i < 10; i++) {
+        /*LoopQueue<Integer> queue = new LoopQueue<>();
+        for (int i = 0;
+        i < 10; i++) {
             queue.enqueue(i);
             System.out.println(queue);
 
@@ -23,7 +55,6 @@ public class Main {
                 queue.dequeue();
                 System.out.println(queue);
             }
-        }
-
+        }*/
     }
 }
